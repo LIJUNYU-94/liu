d71d1374e169b77c46738666fd3bea49be7a63f5
LIJUNYU-94 <24aw0141@jec.ac.jp>
2024-12-04 18:15:48 +0900

    fe

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          shinha is hit for{count} times
+          shinha and lin is hit for{count} times
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
